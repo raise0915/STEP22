e53f2107dbf419a54c47565a4bcdbef35123a3eb
raise0915 <63898148+raise0915@users.noreply.github.com>
2022-05-12 20:54:00 +0900

    0512_提出版

--- week1/homework1-1.py
+++ week1/homework1-1.py
@@ -1,70 +1,83 @@
+#標準入力 / 単語1つ を想定しています
 
-#単語の入力
-original_word = input("Input the word name : ")
-
-original_word = original_word.lower() #小文字に揃える
-random_word = ''.join(sorted(original_word)) #ソートする
-
-
-#辞書ファイルの読み込み
+# 辞書ファイルの読み込み
 textfile = "week1\words.txt"
-f = open(textfile,"r")
+f = open(textfile, "r")
 dictionary = f.read().split("\n")
 f.close()
-    
-#ソートされた新しい辞書の作成 / 元の辞書と紐づけ
-new_dictionary =[]
-for w in dictionary:
-    new_dictionary.append([''.join(sorted(w)).strip(),w.strip()])
-new_dictionary = sorted(new_dictionary,key=lambda x:x[0]) #一番目の要素でソートする
 
-#二分探索
-def binary_search(random_word,new_dictionary):
+#ソートされた新しい辞書の作成 / 元の辞書と紐づけ
+new_dictionary = []
+for w in dictionary:
+    new_dictionary.append([''.join(sorted(w)).strip(), w.strip()])
+new_dictionary = sorted(new_dictionary, key=lambda x: x[0])  # 一番目の要素でソートする
+
+#単語の入力
+def input_check():
+    input_flag = False
+    # アルファベット判定・そうでなければ再度入力を促す
+    while input_flag == False:
+        inputword = input("Input the word name : ")
+        if inputword.isalpha():
+            input_flag = True
+        else:
+            print("Try Again")
+
+
+    original_word= inputword.strip().lower()  # 空白を除去・小文字に揃える
+    inputword = ''.join(sorted(original_word))  # ソートする
+    return inputword,original_word
+
+
+# 二分探索関数
+def binary_search(random_word, new_dictionary):
     left = 0
-    right = len(dictionary) -1
+    right = len(dictionary) - 1
 
     while left <= right:
         middle_index = (left+right) // 2
         middle_value = new_dictionary[middle_index][0]
 
         if random_word < middle_value:
-            right = middle_index -1 
-        
+            right = middle_index - 1
+
         elif random_word > middle_value:
             left = middle_index + 1
 
         else:
-           return middle_index
-    
-    return -1 #anagramが見つからない場合は-1を返す
+            return middle_index
 
+    return -1  # anagramが見つからない場合は-1を返す
 
-num = binary_search(random_word,new_dictionary)
+# ------------ここからメイン------------
 
+random_word,original_word = input_check()
+num = binary_search(random_word, new_dictionary)
+
+# アナグラムが見つからない場合は「Not found」を返して終了
 if num == -1:
     print("Not found")
     exit()
 
 else:
-    anagrams = [] #アナグラムを見つけたときに格納するリスト
+    anagrams = []  # アナグラムを見つけたときに格納するリスト
     anagrams.append(new_dictionary[num][1])
 
-    #前後にも同一のアナグラムが存在するか確認
+    # 前後にも同一のアナグラムが存在するか確認
     center = num - 1
     while random_word == new_dictionary[center][1]:
         anagrams.append(new_dictionary[center][1])
-        center -=1
-    
+        center -= 1
+
     center = num + 1
     while random_word == new_dictionary[center][1]:
         anagrams.append(new_dictionary[center][1])
-        center +=1
+        center += 1
 
-#結果の表示
-print ("=== RESULT ===")
-print("origignal word :" ,original_word)
+# 結果の表示
+print("=== RESULT ===")
+print("origignal word :", original_word)
 print("anagrams :")
 for anagram in anagrams:
     print(anagram)
-print ("=============")
-
+print("=============")
--- week1/homework1-2.py
+++ week1/homework1-2.py
@@ -1,30 +1,35 @@
+# 計算時間は small 8秒, medidum 10分50秒 ,large 3.6時間でした　->1単語1秒程度
+# 実行環境では"week1"のファイル内にテキストデータを格納しています、ご自身の環境に合わせてパスの指定を変えてください
+# inputはテキストファイルからの入力を想定しています
+
 import collections
-import time
+# ------------ファイル指定------------
+# inputファイルの指定
+inputfile = "small.txt"
 
-t1 = time.time()
-#辞書ファイルの読み込み
+# 辞書ファイルの読み込み
 textfile = "week1\words.txt"
+# -----------------------------------
 
-f = open(textfile,"r")
+
+f = open(textfile, "r")
 dictionary = f.read().split("\n")
-dictionary.remove('') #空白が一つ生まれるのでここで駆逐する
+dictionary.remove('')  # 空白が一つ生まれるのでここで駆逐する
 f.close()
-    
+
 #ソートされた新しい辞書の作成 / 元の辞書と紐づけ
-new_dictionary =[]
+new_dictionary = []
 for w in dictionary:
-    new_dictionary.append([w.strip(),collections.Counter(w)])
-new_dictionary = sorted(new_dictionary,key=lambda x:x[0]) #一番目の要素でソートする
+    new_dictionary.append([w.strip(), collections.Counter(w)])
+new_dictionary = sorted(new_dictionary, key=lambda x: x[0])  # 一番目の要素でソートする
 
-
-
-#テキストのinputとカウント
+# テキストのinputと各文字列における文字数のカウント
 def input_texts(file):
-    f = open(file,"r")
+    f = open(file, "r")
     f = f.read().split("\n")
     f.remove('')
 
-    lists=[]
+    lists = []
     for w in f:
         lists.append(collections.Counter(w))
 
@@ -32,67 +37,77 @@ def input_texts(file):
 
 
 #アナグラムの探索 / 最大スコアを持つアナグラムの出力
-def anagram_search(random_word,new_dictionary):
+def anagram_search(random_word, new_dictionary):
 
     ans = 0
     max_anagram = "a"
 
     for word in new_dictionary:
-        counter = word[1]
+        # その文字列のカウント配列を抽出
+        counter = sorted(word[1].most_common(), key=lambda x: x[0])
+
         flag = True
-        for i in range (len(counter)):
-            character = counter.most_common()[i][0]
+
+        # ここで一つずつ探索 -> 文字カウントをそれぞれ二分探索したらもっと早いかも？ただループ文が増えてもっと遅くなる、、、各要素での二分探索がよくわからない、、、と迷ったのでとりあえず線形探索しました
+        for i in range(len(counter)):
+            character = counter[i][0]
 
             # 辞書のある文字のカウントがinputの文字カウントより下回っていたらアナグラムを作成することはできない
-            if counter.most_common()[i][1] > random_word[character]: 
+            if counter[i][1] > random_word[character]:
                 flag = False
                 break
 
         if flag:
-            count=score_calc(word[0])
-     
-            #得点の比較
-            ans = max(ans,count)
+            count = score_calc(word[0])
 
-            #最大スコアが更新されたときに文字列も更新する
+            # 得点の比較
+            ans = max(ans, count)
+
+            # 最大スコアが更新されたときに文字列も更新する
             if count == ans:
                 max_anagram = word[0]
 
-    return ans,max_anagram
+    return ans, max_anagram
 
-
-#スコア計算
+# スコア計算
 def score_calc(anagram):
-    SCORES = [1, 3, 2, 2, 1, 3, 3, 1, 1, 4, 4, 2, 2, 1, 1, 3, 4, 1, 1, 1, 2, 3, 3, 4, 3, 4] #得点表
+    SCORES = [1, 3, 2, 2, 1, 3, 3, 1, 1, 4, 4, 2, 2, 1, 1, 3, 4, 1, 1, 1, 2, 3, 3, 4, 3, 4]  # 得点表
 
     count = 0
-    score_counter=collections.Counter(anagram)
+    score_counter = collections.Counter(anagram)
 
     # SCORESに従って得点を付与
-    for i in range(97,123):
-        count+=score_counter[chr(i)]*SCORES[i-97]
+    for i in range(97, 123):
+        count += score_counter[chr(i)]*SCORES[i-97]
 
     return count
 
+# テキストファイルに出力
+def output(outputname, anagrams):
+    f = open("week1/"+str(outputname)+"_answer.txt", "w")
 
-#テキストファイルに出力
-def output(filename,anagrams):
-    f=open("week1/"+str(filename)+"answer.txt","w")
+    # 最大スコアのアナグラムを書き込む
     for anagram in anagrams:
-        f.write(anagram)
+        f.write(anagram+"\n")
+
     f.close()
 
 
-textfile = "week1\medium.txt"
-a = input_texts(textfile)
+# ------------ここからメイン↓------------
 
-total=0
-anagrams=[]
-for random_word in a:
-    ans,max_anagram=anagram_search(random_word,new_dictionary)
+text = input_texts("week1/"+inputfile)
 
-    total+=ans
+# outputファイル用にinputfileから名前を抽出する
+target = '.txt'
+idx = inputfile.find(target)
+outputname = inputfile[:idx]
+
+anagrams = []
+
+#テキスト内の単語ごとにアナグラム探索・maxスコアを持つアナグラムを配列に格納
+for random_word in text:
+    ans, max_anagram = anagram_search(random_word, new_dictionary)
     anagrams.append(max_anagram)
-print(total)
 
-print(time.time()-t1)
+# ファイルの出力
+output(outputname, anagrams)
