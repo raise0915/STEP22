b4b9f873e4b53ce1ba01ffb0a96451bba01549e7
raise0915 <63898148+raise0915@users.noreply.github.com>
2022-06-01 22:53:24 +0100

    途中経過0601

--- week4/README.md
+++ week4/README.md
@@ -1,4 +1,47 @@
-## はじめに
-大変遅くなってしまって申し訳ないです、、、~~英国時間で水曜日以内という言い訳だけさせてください~~
+# はじめに
+大変遅くなってしまって申し訳ないです、、、~~英国時間で水曜日以内という言い訳だけさせてください~~  
+<br>
+# 宿題の概要
+## 深さ優先探索(DFS)と幅優先探索(BFS)で'Google' から ' 渋谷' までのwikipediaの検索経路が存在するかどうかを判定する<br>
 
-## このプログラムの使い方 
+なぜかえげつなく遅いプログラムが出来てしまったのでご不便をおかけします...
+<br>
+
+## 深さ優先探索(DFS)
+スタック
+
+
+# コードの動かし方
+
+## コードの配置
+your favorite directory<br>
+(好きなところにおいてください<br>
+├── data<br>
+│   ├── graph_small.png<br>
+│   ├── links_small.txt<br>
+│   ├── links.txt<br>
+│   ├── pages_small.txt<br>
+│   └── pages.txt<br>
+├── README.md<br>
+├── homework-4.py<br>
+
+※「your favorite directory」までのパス名をわかるようにしてください
+
+## 実行方法
+Python
+テスト環境：python 3.9.2
+以下のように行ってください
+
+`python homework-4.py`
+
+その後、
+`Choose small(1) or normal(2) : `と出てくるので好きな方の数字を入力してください。
+
+`Type your directory for「data」file : `では「data」フォルダまでのパスを入力してください (例：week4/data)
+
+`Type Start and Target Values (EX:Google 渋谷) : `で最初の単語と最終的に辿り着くか調べたい単語について入力してください。「最初の単語(半角スペース)目標単語」という風に入力してください。
+
+
+
+
+
---
+++ week4/homework-4.py
@@ -0,0 +1,142 @@
+import collections
+ENVIRONMENT = "week4/data"
+
+
+# 深さ優先探索
+def dfs(start_idx,target_idx,links,check):
+  # stack
+  container = collections.deque()
+  container.append(start_idx)
+  print(container)
+
+  # containerの中身が存在しなくなるまで探索を続ける
+  while container:
+    v = container.pop() # 頭から1つ抽出
+
+    # 既に探索済みであればスキップする
+    if check[v]:
+      continue
+
+    check[v]=True # 探索check
+
+    # 境界条件 / 目的のwordが見つかった場合は終了する
+    if v == target_idx:
+      return True
+
+    # 次のリンクを取得・再帰で探索
+    for link in links:
+      if check[link]:
+        continue
+      else:
+        container.append(link)
+
+  # ルートが見つからない場合はFalseを返す
+  return False
+
+# 幅優先探索
+def bfs(start_idx,target_idx,links,check):
+  # que
+  container = collections.deque()
+  container.append(start_idx)
+
+  # container の中身が存在しなくなるまで探索を続ける
+  while container:
+    v = container.popleft() # 左から1つ抽出
+
+    # 既に探索済みであればスキップする
+    if check[v]:
+      continue
+
+    check[v]=True # 探索check
+
+    # 境界条件 / 目的のwordが見つかった場合は終了する
+    if v == target_idx:
+      return True
+
+    for link in links:
+      if check[link]:
+        continue
+      else:
+        container.append(link)
+  # ルートが見つからない場合はFalseを返す
+  return False
+
+#small version か normal version か選択する
+def version_check():  
+    pages = {}
+    links = {}
+    while True:
+      choice = int(input("Choose small(1) or normal(2) : "))
+      if choice == 1:
+        TYPE = '_small.txt'
+        break
+      elif choice == 2 :
+        TYPE = '.txt'
+        break
+      else:
+        print("Type Error : Try Again")
+
+    # linksの読み込みが遅いので待っていてください
+    print("Wait a Minute......")
+
+    with open(ENVIRONMENT+'/pages'+TYPE,encoding="utf-8") as f:
+      for data in f.read().splitlines():
+        page = data.split('\t')
+        # page[0]: id, page[1]: title
+        pages[page[0]] = page[1]
+
+    with open(ENVIRONMENT+'/links'+TYPE,encoding="utf-8") as f:
+      for data in f.read().splitlines():
+        link = data.split('\t')
+        # link[0]: id (from), links[1]: id (to)
+        if link[0] in links:
+          links[link[0]].add(link[1])
+        else:
+          links[link[0]] = {link[1]}
+
+    # 既に調べた項目かcheckしておく
+    item_list = [k for k, v in pages.items()]
+    value_list = [False]*len(item_list)
+    check = dict(zip(item_list, value_list))
+
+    return pages,links,check
+
+def main():
+  ENVIRONMENT = input("Type your directory for「data」file : ")
+  pages,links,check = version_check(ENVIRONMENT)
+  
+  while True:
+    # 開始とターゲットの単語を入力する
+    start_value,target_value = map(str,input('Type Start and Target Values (EX:Google 渋谷) : ').split())
+
+    for k, v in pages.items():
+      if v == start_value:
+        print(start_value, k)
+        start_idx = k
+      if v == target_value:
+        target_idx = k
+        print(target_value, k)
+
+    print(links[start_idx])
+
+    try:
+      ans = dfs(start_idx,target_idx,links,check)
+      break
+    except UnboundLocalError:
+      print("Words are not found. Try again")
+  
+  
+  
+  # ansによる探索結果の出力
+  if ans:
+    print('Found')
+  else:
+    print('Not Found')
+
+
+  
+
+
+
+if __name__ == '__main__':
+  main()
